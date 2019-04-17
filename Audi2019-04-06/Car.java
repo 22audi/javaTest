04fc4ba35c41ee89b14ae72af390425e6266906f
22audi <46402328+22audi@users.noreply.github.com>
2019-04-17 10:07:13 -0400

    Update Car.java

--- Audi2019-04-06/Car.java
+++ Audi2019-04-06/Car.java
@@ -7,3 +7,12 @@ public class Car
       Boolean hasSunRoof;
       
    }
+public void Drive(int y)
+    {
+        System.out.println("The car drove " + y + " miles");
+    }
+public void Speed(int y)
+    {
+        System.out.println("The car drove at a speed of " + y + "Mph");
+    
+    }
